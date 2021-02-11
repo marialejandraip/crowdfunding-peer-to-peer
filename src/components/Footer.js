c3b058542fecd74e3545c8c7d1194f5f98ada3be
marialejandraip <mariaibanez07@gmail.com>
2021-02-11 18:32:45 -0500

    arrow mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,16 @@ import bottomMobile from '../assets/images/mascaras/XMLID 2.png';
 
 export default function Footer({ruta, setRuta, id}) {
   const fixedId = ruta ? ruta : setRuta(0);
-
   return (
     <>
       <img src={bottomMobile} alt="" className={styles.item} />
       <div className={styles.nav} >
-        <Link to={`/dashboard/${ruta}`}><button onClick={() => setRuta(ruta-1)} className={styles.arrow}/></Link>
-        <Link to={`/dashboard/${ruta}`}><button onClick={() => setRuta(ruta+1)} className={styles.arrowR}/></Link>
+        {ruta !== 0 &&
+          <Link to={`/dashboard/${ruta}`}><button onClick={() => setRuta(ruta-1)} className={styles.arrow}/></Link>
+        }
+        {ruta !== 2 &&
+          <Link to={`/dashboard/${ruta}`}><button onClick={() => setRuta(ruta+1)} className={styles.arrowR}/></Link>
+        }
       </div>
     </>
   )
