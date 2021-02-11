6a843abe36c517962cf8c18d7b0a6d086699b54e
marialejandraip <mariaibanez07@gmail.com>
2021-02-11 18:45:08 -0500

    change modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button, Row, Col, Nav, Tab } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 export default function ModalFoundations({show, handleClose, name, info, setData, data}) {
   const initialStateValues = {
