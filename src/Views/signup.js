294641ca1188b87df1849cc32b95221c8c249e32
alefyp <alejndraprz@gmail.com>
2021-02-12 16:30:45 -0500

    Alefycita fixeando styles XD

--- src/Views/signup.js
+++ src/Views/signup.js
@@ -12,7 +12,7 @@ import Child from '../assets/images/Chil.png';
 import lock from '../assets/images/icons/lock.svg';
 import mail from '../assets/images/icons/mail.svg';
 import user from '../assets/images/icons/user.svg';
-
+import styles from './signup.module.css';
 import '../components.css';
 
 export default function Signup() {
@@ -25,47 +25,46 @@ export default function Signup() {
   }
   return (
     <>
-    <div className = "signup"> 
+    <div className={styles.signup}> 
+      <img className = {styles['img-top']} src = {top} alt="" />
+      <img className = "top-desktop" src = {topDesktop} alt="" />
+      <img className = {styles.child} src = {Child} alt="" />
 
-    <img className = "img-top" src = {top} alt="" />
-    
-    <img className = "top-desktop" src = {topDesktop} alt="" />
-    <img className = "child" src = {Child} alt="" />
-
-    <div>
-      <h1 className = "tittle-signup">Crear Cuenta</h1>
-      <div className = "container-signup"> 
-        <form >
-          <div className="form-group">
-          <label>
-            <img src = {user} alt="" className="mail" />
-            Nombre:
-            <input type='text' 
-            onChange={(e) => setName(e.target.value)}/> 
-          </label>
-          <label>
-            <img src = {mail} alt="" className="mail" />
-            Email:
-            <input type='text'
-            onChange={(e) => setEmail(e.target.value)} /> 
-          </label>
-          <label>
-            <img src = {lock} alt="" className="mail" />
-            Contraseña:
-            <input type='password' 
-            onChange={(e) => setPassword(e.target.value)}/> 
-          </label>
-          <Button className = "col-6 button"
-            onClick={() => handleSubmit()}
-            >Registrar</Button>
-          </div>
-        </form>
+      <div className={styles.titleAndForm}>
+        <h1 className = {styles['title-signup']}>Crear Cuenta</h1>
+        <div className = {styles['container-signup']}> 
+          <form >
+            <div className={styles['form-group']}>
+            <label>
+              <img src = {user} alt="" className="mail" />
+              Nombre:
+              <input type='text' 
+              onChange={(e) => setName(e.target.value)}/> 
+            </label>
+            <label>
+              <img src = {mail} alt="" className="mail" />
+              Email:
+              <input type='text'
+              onChange={(e) => setEmail(e.target.value)} /> 
+            </label>
+            <label>
+              <img src = {lock} alt="" className="mail" />
+              Contraseña:
+              <input type='password' 
+              onChange={(e) => setPassword(e.target.value)}/> 
+            </label>
+            <Button className = "col-6 button"
+              onClick={() => handleSubmit()}
+              >Registrar</Button>
+            </div>
+          </form>
+        </div>
       </div>
+    <img className = {styles['image-bottom']} src = {bottom} alt="" />
+    <div className = {styles.arrowSign}>
+      <Link to="/"><button className = {styles.arrow}/></Link>
     </div>
-    <img className = "image-bottom" src = {bottom} alt="" />
-    <Link to="/"><button className = "arrow"/></Link>
-
-    <img className = "bottom-desktop" src = {bottomDesktop} alt="" />
+    <img className = {styles['bottom-desktop']} src = {bottomDesktop} alt="" />
   </div>
   </>
   )
